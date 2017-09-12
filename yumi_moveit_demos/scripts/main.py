87a4573b09281c558de9c93f16820a17c0fb5131
hkaraoguz <hkaraoguz@yahoo.com.tr>
2017-09-12 13:54:54 +0200

    Fixes for motion planning without hitting the joint limits

--- yumi_moveit_demos/scripts/main.py
+++ yumi_moveit_demos/scripts/main.py
@@ -24,7 +24,7 @@ def close_grippers(arm):
     :rtype: None
     """
     yumi.gripper_effort(arm, 15.0)
-    yumi.gripper_effort(arm, 0.0)
+   # yumi.gripper_effort(arm, 0.0)
 
 def open_grippers(arm):
     """Opens the grippers.
@@ -36,24 +36,34 @@ def open_grippers(arm):
     :returns: Nothing
     :rtype: None
     """
-    yumi.gripper_effort(arm, -15.0)
+    yumi.gripper_effort(arm, -10.0)
     yumi.gripper_effort(arm, 0.0)
 
 
 
 def move_and_grasp(arm, pose_ee, grip_effort):
-    try:
-        yumi.traverse_path([pose_ee], arm, 10)
-    except Exception:
-        if (arm == yumi.LEFT):
-            yumi.plan_and_move(yumi.group_l, yumi.create_pose_euler(pose_ee[0], pose_ee[1], pose_ee[2], pose_ee[3], pose_ee[4], pose_ee[5]))
-        elif (arm == yumi.RIGHT):
-            yumi.plan_and_move(yumi.group_r, yumi.create_pose_euler(pose_ee[0], pose_ee[1], pose_ee[2], pose_ee[3], pose_ee[4], pose_ee[5]))
 
-    if (grip_effort <= 20 and grip_effort >= -20):
-        yumi.gripper_effort(arm, grip_effort)
-    else:
-        print("The gripper effort values should be in the range [-20, 20]")
+    #if (grip_effort <= 20 and grip_effort >= -20):
+    #    yumi.gripper_effort(arm, grip_effort)
+    #else:
+    #    print("The gripper effort values should be in the range [-20, 20]")
+   # try:
+   #     yumi.traverse_path([pose_ee], arm, 10)
+   # except Exception as ex:
+   #     rospy.logerr(str(ex))
+   #     return
+
+    if (arm == yumi.LEFT):
+        yumi.plan_and_move(yumi.group_l,
+                           yumi.create_pose_euler(pose_ee[0], pose_ee[1], pose_ee[2], pose_ee[3], pose_ee[4],
+                                                  pose_ee[5]))
+    elif (arm == yumi.RIGHT):
+        yumi.plan_and_move(yumi.group_r,
+                           yumi.create_pose_euler(pose_ee[0], pose_ee[1], pose_ee[2], pose_ee[3], pose_ee[4],
+                                                  pose_ee[5]))
+
+
+    
 
 
 
@@ -82,14 +92,38 @@ def run():
     # Drive YuMi end effectors to a desired position (pose_ee), and perform a grasping task with a given effort (grip_effort)
     # Gripper effort: opening if negative, closing if positive, static if zero
     pose_ee = [0.25, 0.15, 0.2, 0.0, 3.14, 0.0]
+    pose_ee_t = [0.392, 0.0345, 0.2, 0.0, 3.14, 0.0]
     grip_effort = -10.0
+    open_grippers(yumi.LEFT)
+    open_grippers(yumi.RIGHT)
+    move_and_grasp(yumi.LEFT, pose_ee_t, grip_effort)
+    rospy.sleep(2.0)
+    #pose_ee_t[2] = 0.152
+    pose_ee_t[2] = 0.149
+    move_and_grasp(yumi.LEFT, pose_ee_t, grip_effort)
+    rospy.sleep(2.0)
+    close_grippers(yumi.LEFT)
+    pose_ee_t[2] = 0.2
+    move_and_grasp(yumi.LEFT, pose_ee_t, grip_effort)
+    rospy.sleep(2.0)
+    
+    pose_ee = [0.25, 0.15, 0.2, 0.0, 3.14, 0.0]
+    grip_effort = 10.0
     move_and_grasp(yumi.LEFT, pose_ee, grip_effort)
+    rospy.sleep(2.0)
+    open_grippers(yumi.LEFT)
 
     pose_ee = [0.25, -0.25, 0.2, 0.0, 3.14, 0.0]
-    grip_effort = -10.0
+    #grip_effort = -10.0
+    #pose_ee_t[2] = 0.2
     move_and_grasp(yumi.RIGHT, pose_ee, grip_effort)
+    rospy.sleep(2.0)
+    open_grippers(yumi.RIGHT)
+    rospy.sleep(2.0)
+    # Reset YuMi joints to "home" position
+    yumi.reset_pose()
 
-    rospy.spin()
+
 
 
 
